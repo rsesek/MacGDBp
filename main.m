8c5b546f9868a337810e5a7c63ca538c84325c49
Robert Sesek <rsesek@bluestatic.org>
2007-07-31 23:48:59 -0700

    Adding an empty Xcode project with no configruation changes at all

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacGDBp
+//
+//  Created by Robert Sesek on 7/31/07.
+//  Copyright __MyCompanyName__ 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
