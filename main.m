80b28805dd67ee67122e7379c4282c4552cb64e5
Robert Sesek <rsesek@bluestatic.org>
2007-08-01 00:23:08 -0700

    Adding the GPL header to the two existing source files

--- main.m
+++ main.m
@@ -1,10 +1,18 @@
-//
-//  main.m
-//  MacGDBp
-//
-//  Created by Robert Sesek on 7/31/07.
-//  Copyright __MyCompanyName__ 2007. All rights reserved.
-//
+/*
+ * MacGDBp
+ * Copyright (c) 2002 - 2007, Blue Static <http://www.bluestatic.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 
 #import <Cocoa/Cocoa.h>
 
