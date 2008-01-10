e15a3e32da48ef6a0a5c21afa756d62ee80840d8
Robert Sesek <rsesek@bluestatic.org>
2008-01-09 16:11:51 -0800

    Instead of using NSSplitView, use BSSplitView which is our custom implementation which draws a smaller, gradiated divider
    * English.lproj/Debugger.xib: Switch to using a custom class for our two SplitView's
    * Source/BSSplitView.h: New file
    * Source/BSSplitView.m: ditto
    * MacGDBp.xcodeproj: Project updates for new files

---
+++ Source/BSSplitView.m
@@ -0,0 +1,51 @@
+/*
+ * MacGDBp
+ * Copyright (c) 2007 - 2008, Blue Static <http://www.bluestatic.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#import "BSSplitView.h"
+
+
+@implementation BSSplitView
+
+/**
+ * Override the drawing method for the divider so we can make it prettier
+ *
+ * @todo draw the dimple
+ */
+- (void)drawDividerInRect:(NSRect)rect
+{
+	NSColor *startColor = [NSColor colorWithDeviceRed:0.875 green:0.875 blue:0.875 alpha:1.0];
+	NSColor *endColor = [NSColor colorWithDeviceRed:0.812 green:0.812 blue:0.812 alpha:1.0];
+	
+	// draw the gradient
+	NSGradient *gradient = [[NSGradient alloc] initWithStartingColor:startColor endingColor:endColor];
+	[gradient drawInRect:rect angle:([self isVertical] ? 0.0 : 90.0)];
+	
+	// draw the stroke
+	NSColor *stroke = [NSColor colorWithDeviceRed:0.667 green:0.667 blue:0.667 alpha:1.0];
+	[stroke setStroke];
+	[NSBezierPath setDefaultLineWidth:0.5];
+	[NSBezierPath strokeRect:rect];
+}
+
+/**
+ * Returns the size of the divider to draw
+ */
+- (CGFloat)dividerThickness;
+{
+	return 6.0;
+}
+
+@end
