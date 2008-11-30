bc1ccb5c37e2db7070eced715a7a7c59fbc3c3f0
Robert Sesek <rsesek@bluestatic.org>
2008-11-30 18:52:14 -0500

    Adding the StackFrame class
    * Source/StackFrame.m+h: New files
    * MacGDBp.xcodeproj: Project updates

---
+++ Source/StackFrame.h
@@ -0,0 +1,67 @@
+/*
+ * MacGDBp
+ * Copyright (c) 2007 - 2008, Blue Static <http://www.bluestatic.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface StackFrame : NSObject
+{
+	/**
+	 * The position in the stack
+	 */
+	int index;
+	
+	/**
+	 * File the current frame is in
+	 */
+	NSString *filename;
+	
+	/**
+	 * Cached, highlighted version of the source
+	 */
+	NSString *source;
+	
+	/**
+	 * Line number of the source the frame points to
+	 */
+	int lineNumber;
+	
+	/**
+	 * Current-executing function
+	 */
+	NSString *function;
+	
+	/**
+	 * Variable contexts (e.g. locals, class, etc.)
+	 */
+	NSDictionary *contexts;
+}
+
+@property(readwrite) int index;
+@property(readonly, copy) NSString *filename;
+@property(readonly, copy) NSString *source;
+@property(readwrite) int lineNumber;
+@property(readwrite, copy) NSString *function;
+@property(readonly, copy) NSDictionary *contexts;
+
+- (id)initWithIndex:(int)anIndex
+	   withFilename:(NSString *)aFilename
+		 withSource:(NSString *)aSource
+			 atLine:(int)aLineNumber
+		 inFunction:(NSString *)function
+	   withContexts:(NSDictionary *)contexts;
+
+@end
---
+++ Source/StackFrame.m
@@ -0,0 +1,55 @@
+/*
+ * MacGDBp
+ * Copyright (c) 2007 - 2008, Blue Static <http://www.bluestatic.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#import "StackFrame.h"
+
+/**
+ * Private class continuation
+ */
+@interface StackFrame()
+@property(readwrite, copy) NSString *filename;
+@property(readwrite, copy) NSString *source;
+@property(readwrite, copy) NSDictionary *contexts;
+@end
+/***/
+
+@implementation StackFrame
+
+@synthesize index, filename, source, lineNumber, function, contexts;
+
+/**
+ * Constructor
+ */
+- (id)initWithIndex:(int)anIndex
+	   withFilename:(NSString *)aFilename
+		 withSource:(NSString *)aSource
+			 atLine:(int)aLineNumber
+		 inFunction:(NSString *)aFunction
+	   withContexts:(NSDictionary *)aContexts
+{
+	if (self = [super init])
+	{
+		self.index		= anIndex;
+		self.filename	= aFilename;
+		self.source		= aSource;
+		self.lineNumber	= aLineNumber;
+		self.function	= aFunction;
+		self.contexts	= aContexts;
+	}
+	return self;
+}
+
+@end
