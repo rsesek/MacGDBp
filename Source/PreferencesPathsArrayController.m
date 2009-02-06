6af4400bf62165deeaf33bfed56102986b72695c
Robert Sesek <rsesek@bluestatic.org>
2009-02-06 00:57:00 -0500

    Add a custom NSArrayController for the paths preferences so we can customize the default dictionary values
    * English.lproj/Preferences.xib: Update the bindings
    * Source/PreferencesPathsArrayController.h+m: New files
    * MacGDBp.xcodeproj/project.pbxproj: Project updates for new files

---
+++ Source/PreferencesPathsArrayController.m
@@ -0,0 +1,33 @@
+/*
+ * MacGDBp
+ * Copyright (c) 2007 - 2009, Blue Static <http://www.bluestatic.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#import "PreferencesPathsArrayController.h"
+
+
+@implementation PreferencesPathsArrayController
+
+/**
+ * Subclass to add the default values to the NSDictionary
+ */
+- (id)newObject
+{
+	NSMutableDictionary *obj = (NSMutableDictionary *)[super newObject];
+	[obj setValue:@"Remote Path" forKey:@"remote"];
+	[obj setValue:@"Local Path"	forKey:@"local"];
+	return obj;
+}
+
+@end
